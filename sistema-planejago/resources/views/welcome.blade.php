6ffe7cc2ec226f88406a8dd381a7aa97f2309e70
GabrielVitz <gabriel.andruchevitz@gmail.com>
2026-04-11 18:07:48 -0400

    feat: configuracao inicial do projeto

---
+++ sistema-planejago/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
+        <title> Sistema PlanejaGo </title>
+
+    </head>
+    <body class="flex flex-col w-full h-screen justify-center items-center">
+        <p class= "font-roboto font-bold">Projeto Configurado</p>
+    </body>
+</html>
